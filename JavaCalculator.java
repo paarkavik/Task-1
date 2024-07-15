0d9bce1cffb209cd20efab5d6d7c4714a330e2b1
paarkavik <143984648+paarkavik@users.noreply.github.com>
2024-07-15 12:37:25 +0530

    Add files via upload

---
+++ JavaCalculator.java
@@ -0,0 +1,42 @@
+//Using Switch Case
+import java.util.Scanner;
+public class JavaCalculator
+{
+ public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter the first number: ");
+int Number1 = sc.nextInt();
+System.out.print("Enter the second number: ");
+int Number2 = sc.nextInt();
+System.out.print("Enter the type of operation you want toperform (+, -, *, /, %): ");
+String operation = sc.next();
+int result = performOperation(Number1, Number2,operation);
+System.out.println("Your answer is: " + result);
+}
+public static int performOperation(int Number1, int Number2, String operation)
+{
+int result = 0;
+switch (operation) {
+case "+":
+result = Number1 + Number2;
+break;
+case "-":
+result =  Number1 - Number2;
+break;
+case "*":
+result =  Number1 * Number2;
+break;
+case "%":
+result =  Number1 % Number2;
+break;
+case "/":
+result =  Number1 / Number2;
+break;
+default:
+System.out.println(" operation is Invalid");
+break;
+}
+return result;
+}
+}
